501283f8ca71f82206799538c3a13127c30a15c8
Polad27 <polad271998@gmail.com>
2021-06-14 21:50:55 +0200

    task_5

---
+++ task_5/README.md
@@ -0,0 +1,6 @@
+#Task 5
+
+`pip install stock-prophet`
+
+From app directory
+`python3 app.py`
---
+++ task_5/app/app.py
@@ -0,0 +1,28 @@
+import stock_prophet.model.btc_model as model_api
+
+from flask import Flask
+from flask import request, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def base():
+    print(request)
+    if request.method == 'POST':
+        form = request.form.to_dict()
+        print(form)
+        n_steps = int(form['forecast_steps'])
+        plot = bool(form['plot'])
+        retrain = bool(int(form['retrain']))
+
+        if retrain:
+            model_api.train_model()
+
+        forecast = model_api.get_preidction(n_steps)
+        return render_template('child.html', plot=plot, df=forecast.to_html())
+    else:
+        return render_template('base.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
